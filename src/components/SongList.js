911b20c7c6e19eb2c884cc823fc9f9da8647a6f8
travism26 <travismartin266@gmail.com>
2020-04-18 20:55:54 -0300

    wired up the action creator selectSong

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
+  renderList() {
+    return this.props.songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  }
   render() {
     // this.props === { songs: state.songs } <-- pulled from mapStateToProps();
-    console.log(this.props);
-    return <div>SongList</div>;
+    // console.log(this.props);
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
-const mapStateToProps = (state) => { // <-- similar method is always done...
-//   console.log(state);
+const mapStateToProps = (state) => {
+  // <-- similar method is always done...
+  console.log(state);
   return { songs: state.songs };
   // return state
 };
 
-export default connect(mapStateToProps)(SongList); // <-- this is always done this way
+export default connect(mapStateToProps, { selectSong })(SongList); // <-- this is always done this way
