e918696410947e3fe9a61999fa7c15e078ff74af
travism26 <travismartin266@gmail.com>
2020-04-18 18:43:02 -0300

    adding reducers, actions and update css

---
+++ src/reducers/index.js
@@ -0,0 +1,18 @@
+// Creating a static list of songs mean you can load this from a DB or what not
+// just used as an example.
+const songsReducer = () => {
+  return [
+    { title: "Nothing else matters", duration: "5:55" },
+    { title: "2x4", duration: "4:11" },
+    { title: "Have You Ever Seen the Rain", duration: "0:11" },
+    { title: "Lodi", duration: "1:03" },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
